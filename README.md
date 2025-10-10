1cbe9afa51199867ce5588700318a40184b52a79
Beforerr <zzj956959688@gmail.com>
2025-10-09 17:50:36 -0700

    feat: init features

--- README.md
+++ README.md
@@ -5,3 +5,17 @@
 [![Build Status](https://github.com/JuliaSpacePhysics/VelocityDistributionFunctions.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/JuliaSpacePhysics/VelocityDistributionFunctions.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/JuliaSpacePhysics/VelocityDistributionFunctions.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/JuliaSpacePhysics/VelocityDistributionFunctions.jl)
 [![Aqua](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
+
+## Features
+
+From velocity distribution functions, generate:
+
+- [ ] Energy/gyro-phase spectra
+- [ ] Pitch-angle distributions (PADs)
+- [ ] Moments
+
+## Elsewhere
+
+- [ISEE_3D](https://earth-planets-space.springeropen.com/articles/10.1186/s40623-017-0761-9): Visualization tool for three-dimensional plasma velocity distributions
+- [Bulk Flow Velocity Changing Software - Lynn B. Wilson](https://wind.nasa.gov/docs/vbulk_change_documentation.pdf)
+- [SPEDAS Particle Tools Development Guide](https://spedas.org/presentations/pgs_development_v1.1.pdf)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,5 +6,67 @@ using Aqua
     @testset "Code quality (Aqua.jl)" begin
         Aqua.test_all(VelocityDistributionFunctions)
     end
-    # Write your tests here.
+
+    @testset "pitch_angle_distribution" begin
+        @testset "default bins mean" begin
+            flux = fill(Float64(NaN), 1, 1, 1, 3)
+            flux[1, 1, 1, 1] = 2.0
+            flux[1, 1, 1, 2] = 4.0
+            flux[1, 1, 1, 3] = 6.0
+            B = [0.0, 0.0, -1.0]
+            phi = [0.0]
+            theta = [0.0, 90.0, 180.0]
+
+            pad = pitch_angle_distribution(flux, B, phi, theta)
+            data = pad.data
+
+            @test size(data) == (1, 12, 1)
+            @test data[1, 1, 1] == 2.0
+            @test data[1, 6, 1] == 4.0
+            @test data[1, 12, 1] == 6.0
+
+            for idx in setdiff(collect(1:12), [1, 6, 12])
+                @test isnan(data[1, idx, 1])
+            end
+
+            counts = pad.counts
+            @test counts[1, 1, 1] == 1
+            @test counts[1, 6, 1] == 1
+            @test counts[1, 12, 1] == 1
+        end
+
+        @testset "custom edges sum" begin
+            flux = ones(Float64, 1, 1, 2, 2)
+            B = [0.0, 0.0, -1.0]
+            phi = [0.0, 180.0]
+            theta = [0.0, 180.0]
+            edges = [0.0, 90.0, 180.0]
+
+            pad = pitch_angle_distribution(flux, B, phi, theta; angles = edges, method = :sum)
+            data = pad.data
+            @test size(data) == (1, 2, 1)
+            @test data[1, 1, 1] == 2.0
+            @test data[1, 2, 1] == 2.0
+            @test pad.counts[1, 1, 1] == 2
+            @test pad.counts[1, 2, 1] == 2
+            @test pad.edges == edges
+        end
+
+        @testset "time dependent inputs" begin
+            flux = ones(Float64, 2, 1, 2, 2)
+            B = [0.0 0.0 -1.0; 0.0 0.0 1.0]
+            phi = reshape([0.0, 180.0], 1, :)
+            theta = reshape([0.0, 180.0], 1, :)
+
+            pad = pitch_angle_distribution(flux, B, phi, theta; angles = 2, method = :mean)
+            data = pad.data
+            counts = pad.counts
+
+            @test size(data) == (2, 2, 1)
+            @test all(data[1, :, 1] .== 1.0)
+            @test all(data[2, :, 1] .== 1.0)
+            @test all(counts .== 2)
+            @test pad.edges == collect(0.0:90.0:180.0)
+        end
+    end
 end
